8922c2dc5ecadfb1baa6a7afc8b860d5aedad345
DavidBurt2 <david.r.burt@williams.edu>
2018-02-19 17:31:06 +0000

    Update poly_inverse_time_decay.py

--- LeNet-All Standard Dropout/poly_inverse_time_decay.py
+++ LeNet-All Standard Dropout/poly_inverse_time_decay.py
@@ -10,7 +10,6 @@ from tensorflow.python.framework import ops
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import random_ops
-from tensorflow.python.util.tf_export import tf_export
 
 def poly_inverse_time_decay(learning_rate,
                        global_step,
