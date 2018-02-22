8a9c5a9a9f38fad85f69f8b5dde73a6516c63b1b
DavidBurt2 <david.r.burt@williams.edu>
2018-02-22 18:51:51 +0000

    Update evaluation.sh

--- evaluation.sh
+++ evaluation.sh
@@ -1,16 +1,21 @@
 
 #USE ABSOLUTE PATHS THROUGHOUT, add this to cron tab, set it to run every couple minutes, ie */2 * * * * ./~/MLSALT4/evaluation.sh
+#THIS SHOULD WORK IF checkpoints are saved to: checkpoints/model.ckpt+"-"+str(i), needs a bit of cleaning up to succesfully remove and move checkpoints
+#!/bin/bash
 
-for f in $(ls -1v checkpoints/*)
+for f in $(ls -1v checkpoints/*.index)
 do
-	n=${f#checkpoints/model.ckpt-}
+	check=${f%.*}
+	n=${check#checkpoints/model.ckpt-}
+	echo $check
 	echo $n >> trainAccuracies.log
-	python evaluate_mc_dropout.py --checkpoint_file_path $f >> trainAccuracies.log
-  echo "\n"
-	if (( $n % 100000 == 0 )) 
-	then
-		mv $f checkpoints.save/
-	else
-		rm $f
-	fi
+	python evaluate_mc_dropout.py --checkpoint_file_path $check >> trainAccuracies.log
 done
+  #echo "\n"
+	#if (( $n % 100000 == 0 )) 
+	#then
+	#	mv $f checkpoints.save/
+	#else
+		#rm $f
+	#fi
+#done
