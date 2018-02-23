6d115132415fc32a6a1e3c2c4c49be02de3d45cd
DavidBurt2 <david.r.burt@williams.edu>
2018-02-23 15:31:22 +0000

    Update evaluation.sh

--- lenet-all-standard-dropout/evaluation.sh
+++ lenet-all-standard-dropout/evaluation.sh
@@ -12,7 +12,7 @@ do
 	echo $check
 	echo $n >> trainingAccuracies.log
 	python3 evaluate_mc_dropout_py3.py --checkpoint_file_path $check >> process.log
-	if (( $n % 10000 == 0 )) 
+	if (( $n % 100000 == 0 )) 
 	then
 		for file in $(ls -1v $check*)
 			do
