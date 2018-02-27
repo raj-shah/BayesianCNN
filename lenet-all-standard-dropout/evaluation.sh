cc8a49e52fa8953fb10b7bf1a7a75bd3df200c8c
DavidBurt2 <david.r.burt@williams.edu>
2018-02-27 14:04:48 +0000

    changed to just move files without evaluating

--- lenet-all-standard-dropout/evaluation.sh
+++ lenet-all-standard-dropout/evaluation.sh
@@ -1,24 +1,27 @@
 
-#USE ABSOLUTE PATHS THROUGHOUT, add this to cron tab, set it to run every couple minutes, ie */2 * * * * ./~/MLSALT4/evaluation.sh
-#THIS SHOULD WORK IF checkpoints are saved to: checkpoints/model.ckpt+"-"+str(i), needs a bit of cleaning up to succesfully remove and move checkpoints
+#1 Make the directory (using 'mkdir checkpoints.save') checkpoints.save in the same directory as this file
+#2 in command line type 'EDITOR=nano crontab -e', this will open a buffer
+#2 in the buffer, type */3 * * * * ./PATH-TO/evaulation.sh, use the absolute path to this script here, save and close the crontab buffer, this will run the script every 3 minutes
+#3 type in crontab -l, this should return */3 * * * * ./evaulation.sh, indicating theat the cronjob has been set
+#4 Train the network, the evaluation script will be run every 3 minutes, moving stuff over to a new directory
 #!/bin/bash
 
-#mkdir checkpoints.save
+mkdir checkpoints.save
 
-for f in $(ls -1v checkpoints/*.index)
+for file in $(ls -1v checkpoints/*)#.index)
 do
-	check=${f%.*}
-	n=${check#checkpoints/model.ckpt-}
-	echo $check
-	echo $n >> trainingAccuracies.log
-	python3 evaluate_mc_dropout_py3.py --checkpoint_file_path $check >> process.log
-	#if (( $n % 100000 == 0 )) 
+	#check=${f%.*}
+	#n=${check#checkpoints/model.ckpt-}
+	#echo $check
+	#echo $n >> trainingAccuracies.log
+	#python3 evaluate_mc_dropout_py3.py --checkpoint_file_path $check >> process.log
+	#if (( $n % 10000 == 0 )) #This is currently trivially sa
 	#then
-	#	for file in $(ls -1v $check*)
-		#	do
-		#	mv $file checkpoints.save/
-		#	echo $check | mail -s "Evaluations script" david.r.burt94@gmail.com # PLEASE CHANGE EMAIL
-		#	done
+		#for file in $(ls -1v $check*)
+			#do
+	mv -f $file checkpoints.save/
+			#echo $check | mail -s "Evaluations script" david.r.burt94@gmail.com # PLEASE CHANGE EMAIL
+			#done
 	#else
 	#	for file in $(ls -1v $check*)
 	#		do
