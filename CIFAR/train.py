3ff0c97f9935cc1b61b30832b3ec825d40c407c1
raj-shah <rajshah@outlook.ie>
2018-03-14 10:41:31 +0000

    Update train.py
    Added batch testing for every 1000 training iterations

--- CIFAR/train.py
+++ CIFAR/train.py
@@ -66,10 +66,20 @@ if __name__ == '__main__':
             logger.info("Global step: {}    Loss: {}    Accuracy: {}".format(global_step, loss, acc))
 
             if global_step % checkpoint_freq == 0:
-                val_acc = sess.run([model.val_acc], feed_dict={
-                    model.x_: test_x,
-                    model.y: test_y
-                })
+                
+                test_batch_acc_total = 0
+                test_batch_count = 0
+                
+                for test_feature_batch, test_label_batch in batch_features_labels(images_test, labels_test, batch_size=100):
+                    test_batch_acc_total += sess.run([model.val_acc], feed_dict={
+                    model.x_: test_feature_batch,
+                    model.y: test_label_batch
+                    keep_prob: 1.0
+                    })
+                    test_batch_count += 1
+
+                val_acc = test_batch_acc_total/test_batch_count
+                
                 logger.info("EVALUATION STEP:\tTest Accuracy: {}".format(val_acc))
                 saver.save(sess, checkpoint_path, global_step=global_step)
 
