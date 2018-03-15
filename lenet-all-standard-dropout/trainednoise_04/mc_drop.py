1d1329734261cbad16c3e55880ebe94be5802b75
sjonnii <36279828+sjonnii@users.noreply.github.com>
2018-03-15 13:17:05 +0000

    Add files via upload

---
+++ lenet-all-standard-dropout/trainednoise_04/mc_drop.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from Model import Model
+import numpy as np
+
+FLAGS = tf.app.flags.FLAGS
+
+
+def evaluate():
+	with tf.Graph().as_default():
+
+		mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+		x_ = tf.placeholder(tf.float32, shape=[None, 784]) #data gets loaded as a 28x8 vector
+		x = tf.reshape(x_,[-1,28,28,1],name = 'x')   #mnist dataset is shape 28,28,1
+		y = tf.placeholder(shape=[None, 10], dtype=tf.float32, name='y') #10 labels
+		softmax_tensors = tf.placeholder(tf.float32, shape=[FLAGS.T, 10000, 10])
+	
+		model = Model()
+		logits = model.inference(x, keep_prob=0.5)
+		softmax = tf.nn.softmax(logits)
+		shape = tf.shape(softmax)
+		saver = tf.train.Saver(max_to_keep = None)
+
+		mean = tf.reduce_mean(softmax_tensors,0)
+		accuracy = model.accuracy(mean,y)
+
+		softmax_list = []
+		with tf.Session() as sess:
+			tf.global_variables_initializer().run()
+			saver.restore(sess, FLAGS.checkpoint_file_path)
+			for i in range(FLAGS.T):
+				softmaxi = sess.run([softmax],
+				                            feed_dict={x_: mnist.test.images, y: mnist.test.labels})
+				softmax_list.append(softmaxi)
+				
+			#mean_prob = sess.run([mean], feed_dict = {softmax_tensors: np.squeeze(np.array(softmax_list))})
+
+			total_accuracy = sess.run([accuracy],
+						feed_dict={softmax_tensors: np.squeeze(np.array(softmax_list)), y: mnist.test.labels})
+			print('Test accuracy: {}'.format(total_accuracy))
+
+
+			
+			
+
+def main(argv=None):
+    evaluate()
+
+if __name__ == '__main__':
+    tf.app.flags.DEFINE_string('checkpoint_file_path', 'checkpoints/model.ckpt-10000000', 'path to checkpoint file')
+    tf.app.flags.DEFINE_integer('T', 10, 'Number of Forward Passes')
+    tf.app.run()
---
+++ lenet-all-standard-dropout/trainednoise_04/train.py
@@ -0,0 +1,77 @@
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from Model import Model
+import math
+import numpy as np
+
+FLAGS = tf.app.flags.FLAGS
+
+def train():
+    model = Model()
+
+    with tf.Graph().as_default():
+
+	noiseLevel = '0.4'
+
+	mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+	labels = mnist.test.labels
+	images = np.load(noiseLevel+'.npy')
+	images = data.reshape([60000,28,28,1])
+
+        x = tf.placeholder(tf.float32, shape=[-1, 28,28,1]) #data gets loaded as a 28x8 vector
+        #x = tf.reshape(x_,[-1,28,28,1],name = 'x')   #mnist dataset is shape 28,28,1
+        y = tf.placeholder(shape=[None, 10], dtype=tf.float32, name='y') #10 labels
+        keep_prob = tf.placeholder(tf.float32, name='dropout_prob')
+        global_step = tf.contrib.framework.get_or_create_global_step()
+
+        logits = model.inference(x, keep_prob=keep_prob)
+        loss = model.loss(logits=logits, labels=y)
+
+        accuracy = model.accuracy(logits, y)
+        summary_op = tf.summary.merge_all()
+        train_op = model.train(loss, global_step=global_step)
+
+        batch_size = FLAGS.batch_size #batch size, this might not be correct size
+        input_size = 60000
+        porp = int(math.ceil(input_size/batch_size))
+
+        init = tf.global_variables_initializer() 
+        saver = tf.train.Saver(max_to_keep = 100000)
+
+
+        with tf.Session(config=tf.ConfigProto(log_device_placement=False)) as sess:
+            writer = tf.summary.FileWriter(FLAGS.summary_dir, sess.graph)
+            sess.run(init)
+            for i in range(FLAGS.num_iter):
+                if i%(porp) == 0:
+                    permutation=np.random.permutation(input_size) #create a list with random indexes
+                    increment = 0 #restart the increment variable 
+
+                batch_idx = permutation[increment*batch_size:(increment+1)*batch_size]
+                increment += 1
+                image_batch=images[batch_idx] #this is a list with batch size number of elements. Each element is a (32,32,3) array (images)
+                label_batch=labels[batch_idx]
+
+                _, cur_loss, summary = sess.run([train_op, loss, summary_op],
+                                                feed_dict={x_: image_batch, y: label_batch, keep_prob: 0.5})
+
+                writer.add_summary(summary, i)
+                if i % 5000 == 0:
+                    f = open('trainingStdDrop.log', 'a+')
+                    validation_accuracy = accuracy.eval(feed_dict={x_: mnist_.test.images, y: mnist_.test.labels, keep_prob: 1.0}) 
+                    f.write('{}, {}, {} \n'.format(i, cur_loss, validation_accuracy))
+                    f.close()
+                    saver.save(sess, FLAGS.checkpoint_file_path+"-"+str(i))
+
+def main(argv=None):
+    train()
+
+
+if __name__ == '__main__':
+    tf.app.flags.DEFINE_integer('batch_size', 64, 'size of training batches')
+    tf.app.flags.DEFINE_integer('num_iter', 250000, 'number of training iterations')
+    tf.app.flags.DEFINE_string('checkpoint_file_path', 'checkpoints/model.ckpt', 'path to checkpoint file')
+    tf.app.flags.DEFINE_string('summary_dir', 'graphs', 'path to directory for storing summaries')
+
+    tf.app.run()
