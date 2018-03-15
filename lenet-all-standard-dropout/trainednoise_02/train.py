9b2033665e8ee97070c28fa432e1a47e0aaf206d
sjonnii <36279828+sjonnii@users.noreply.github.com>
2018-03-15 14:03:38 +0000

    Add files via upload

--- lenet-all-standard-dropout/trainednoise_02/train.py
+++ lenet-all-standard-dropout/trainednoise_02/train.py
@@ -8,61 +8,62 @@ import numpy as np
 FLAGS = tf.app.flags.FLAGS
 
 def train():
-    model = Model()
+	model = Model()
 
-    with tf.Graph().as_default():
+	with tf.Graph().as_default():
 
-	noiseLevel = '0.2'
+		mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+		labels = mnist.train.labels
+		images = np.load('0.2.npy')
+		images = images.reshape([55000,28,28,1])
+		eval_data = mnist.test.images
+		eval_data = eval_data.reshape([10000,28,28,1])
 
-	mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-	labels = mnist.test.labels
-	images = np.load(noiseLevel+'.npy')
-	images = data.reshape([60000,28,28,1])
+		x = tf.placeholder(tf.float32, shape=[None, 28,28,1]) #data gets loaded as a 28x8 vector
+		#x = tf.reshape(x_,[-1,28,28,1],name = 'x')   #mnist dataset is shape 28,28,1
 
-        x = tf.placeholder(tf.float32, shape=[-1, 28,28,1]) #data gets loaded as a 28x8 vector
-        #x = tf.reshape(x_,[-1,28,28,1],name = 'x')   #mnist dataset is shape 28,28,1
-        y = tf.placeholder(shape=[None, 10], dtype=tf.float32, name='y') #10 labels
-        keep_prob = tf.placeholder(tf.float32, name='dropout_prob')
-        global_step = tf.contrib.framework.get_or_create_global_step()
+		y = tf.placeholder(shape=[None, 10], dtype=tf.float32, name='y') #10 labels
+		keep_prob = tf.placeholder(tf.float32, name='dropout_prob')
+		global_step = tf.contrib.framework.get_or_create_global_step()
 
-        logits = model.inference(x, keep_prob=keep_prob)
-        loss = model.loss(logits=logits, labels=y)
+		logits = model.inference(x, keep_prob=keep_prob)
+		loss = model.loss(logits=logits, labels=y)
 
-        accuracy = model.accuracy(logits, y)
-        summary_op = tf.summary.merge_all()
-        train_op = model.train(loss, global_step=global_step)
+		accuracy = model.accuracy(logits, y)
+		summary_op = tf.summary.merge_all()
+		train_op = model.train(loss, global_step=global_step)
 
-        batch_size = FLAGS.batch_size #batch size, this might not be correct size
-        input_size = 60000
-        porp = int(math.ceil(input_size/batch_size))
+		batch_size = FLAGS.batch_size #batch size, this might not be correct size
+		input_size = 55000
+		porp = int(math.ceil(input_size/batch_size))
 
-        init = tf.global_variables_initializer() 
-        saver = tf.train.Saver(max_to_keep = 100000)
+		init = tf.global_variables_initializer() 
+		saver = tf.train.Saver(max_to_keep = 100000)
 
 
-        with tf.Session(config=tf.ConfigProto(log_device_placement=False)) as sess:
-            writer = tf.summary.FileWriter(FLAGS.summary_dir, sess.graph)
-            sess.run(init)
-            for i in range(FLAGS.num_iter):
-                if i%(porp) == 0:
-                    permutation=np.random.permutation(input_size) #create a list with random indexes
-                    increment = 0 #restart the increment variable 
+		with tf.Session(config=tf.ConfigProto(log_device_placement=False)) as sess:
+			writer = tf.summary.FileWriter(FLAGS.summary_dir, sess.graph)
+			sess.run(init)
+			for i in range(FLAGS.num_iter):
+				if i%(porp) == 0:
+				    permutation=np.random.permutation(input_size) #create a list with random indexes
+				    increment = 0 #restart the increment variable 
 
-                batch_idx = permutation[increment*batch_size:(increment+1)*batch_size]
-                increment += 1
-                image_batch=images[batch_idx] #this is a list with batch size number of elements. Each element is a (32,32,3) array (images)
-                label_batch=labels[batch_idx]
+				batch_idx = permutation[increment*batch_size:(increment+1)*batch_size]
+				increment += 1
+				image_batch=images[batch_idx] #this is a list with batch size number of elements. Each element is a (32,32,3) array (images)
+				label_batch=labels[batch_idx]
 
-                _, cur_loss, summary = sess.run([train_op, loss, summary_op],
-                                                feed_dict={x_: image_batch, y: label_batch, keep_prob: 0.5})
+				_, cur_loss, summary = sess.run([train_op, loss, summary_op],
+							        feed_dict={x: image_batch, y: label_batch, keep_prob: 0.5})
 
-                writer.add_summary(summary, i)
-                if i % 5000 == 0:
-                    f = open('trainingStdDrop.log', 'a+')
-                    validation_accuracy = accuracy.eval(feed_dict={x_: mnist_.test.images, y: mnist_.test.labels, keep_prob: 1.0}) 
-                    f.write('{}, {}, {} \n'.format(i, cur_loss, validation_accuracy))
-                    f.close()
-                    saver.save(sess, FLAGS.checkpoint_file_path+"-"+str(i))
+				writer.add_summary(summary, i)
+				if i % 5000 == 0:
+				    f = open('trainingStdDrop.log', 'a+')
+				    validation_accuracy = accuracy.eval(feed_dict={x: eval_data, y: mnist.test.labels, keep_prob: 1.0}) 
+				    f.write('{}, {}, {} \n'.format(i, cur_loss, validation_accuracy))
+				    f.close()
+				    saver.save(sess, FLAGS.checkpoint_file_path+"-"+str(i))
 
 def main(argv=None):
     train()
