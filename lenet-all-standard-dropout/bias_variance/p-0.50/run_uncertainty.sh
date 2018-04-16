ad31c52a33291b498ac73df6d1563bc95f91542d
raj-shah <rajshah@outlook.ie>
2018-04-16 14:09:41 +0100

    Create run_uncertainty.sh

---
+++ lenet-all-standard-dropout/bias_variance/p-0.50/run_uncertainty.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for f in 1 2 3 4 5
+do
+for t in 50
+do
+for p in 0.25 .5 .6 .75 .9
+do
+    
+    python uncertainty.py --checkpoint_file_path checkpoints/model.ckpt-5000000 --T $t --prob $p
+done
+done
+done
